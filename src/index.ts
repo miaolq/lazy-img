8e846b6a4e311949fd197bf127ef1d707a74f46d
linqiang <linqiang.miao@gmail.com>
2023-03-06 22:37:56 +0800

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,3 @@
 import LazyImg from './lazy-img/lazy-img'
-export {
-  ReactImgProps,
-  LoadingType,
-  customObserver,
-  cbKey,
-} from './lazy-img/lazy-img'
+export { ReactImgProps, LoadingType, customObserver } from './lazy-img/lazy-img'
 export default LazyImg
